e501cb4c37ef9da11e04c30ec6b5957ffc5a5fe8
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-13 20:39:53 +0900

    이전기능 버튼 컴포넌트 생성,

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -4,5 +4,13 @@ import { useNavigate } from 'react-router-dom';
 export default function BackButton() {
   const navigate = useNavigate();
 
-  return <button>이전으로</button>;
+  return (
+    <button
+      onClick={() => {
+        navigate(-1);
+      }}
+    >
+      이전으로
+    </button>
+  );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../components/BackButton';
 
 function Menu() {
-  return <div>Menu</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>Menu</div>
+      <BackButton>이전으로</BackButton>
+      <button
+        onClick={() => {
+          navigate('Menu/:takeOutId/OrderCheck');
+        }}
+      >
+        {' '}
+        주문하기
+      </button>
+    </div>
+  );
 }
 
 export default Menu;
