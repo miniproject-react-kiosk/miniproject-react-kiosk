a3214ef4bef12125207bb260337da4ad4fbe2137
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-14 23:26:33 +0900

    상단 버튼기능. 3번 페이지 ui 완성

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,32 +3,55 @@ import { useNavigate } from 'react-router-dom';
 import { BsBackspace } from 'react-icons/bs';
 import styled from 'styled-components';
 
+const StAll = styled.div`
+  /* background-color: yellow; */
+  text-align: center;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 20px 0px 20px;
+  margin-top: 20px;
+  height: 30px;
+  z-index: 6;
+`;
+const StBox = styled.div`
+  width: 50px;
+  height: 30px;
+  line-height: 30px;
+  /* border: solid gray 1px; */
+  border-radius: 15px;
+`;
+
+const StBackButton = styled.div`
+  border-radius: 15px;
+
+  cursor: pointer;
+  &:hover {
+    background-color: #f3dc0c;
+    transition: all 300ms ease;
+  }
+  &:hover .BackButton {
+    transition: all 300ms ease;
+    transform: rotate(5deg) scale(1.1);
+  }
+`;
+
 export default function BackButton() {
   const navigate = useNavigate();
 
-  const StBackButton = styled.div`
-    border: solid 2px white;
-    border-radius: 5vw;
-    font-size: 2vw;
-    width: 100px;
-    height: 100px;
-
-    text-align: center;
-
-    cursor: pointer;
-    &:hover {
-      background-color: #f3dc0c;
-      transition: all 300ms ease;
-    }
-  `;
-
   return (
-    <StBackButton
-      onClick={() => {
-        navigate(-1);
-      }}
-    >
-      <BsBackspace />
-    </StBackButton>
+    <StAll>
+      <div>Logo</div>
+      <StBox>
+        <StBackButton
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <BsBackspace className='BackButton' />
+        </StBackButton>
+      </StBox>
+    </StAll>
   );
 }
