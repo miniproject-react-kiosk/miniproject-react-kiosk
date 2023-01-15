737ebbf1cfbc613274405c1c84922513c5116149
yrk721 <yoorae0721@naver.com>
2023-01-15 21:34:17 +0900

    장바구니 추가 test

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -18,7 +18,6 @@ function Menu() {
     <div>
       <div>Menu</div>
       <BackButton>이전으로</BackButton>
-
       <button
         onClick={() => {
           navigate(`/Menu/OrderChoice/${param.takeOutId}`);
@@ -27,12 +26,11 @@ function Menu() {
         {" "}
         주문하기
       </button>
-
       {/* 메뉴추가 */}
       <StMenuContainer>
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="row row-cols-1 row-cols-md-5 g-4">
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1653436847042.png"
                 class="card-img-top"
@@ -45,17 +43,16 @@ function Menu() {
                 </StCardTitle>
                 <StCardText>
                   <p class="card-text">
-                    100% 통닭다리살 겉바속촉 케이준 치킨 패티, <br></br>촉촉한
-                    포테이토 브리오쉬 번, 스페셜 스모키 소스가 선사하는{" "}
-                    <br></br>
-                    놀랍도록 새로운 맛의 치킨 버거!
+                    100% 통닭다리살 겉바속촉 케이준 치킨 패티, 촉촉한 포테이토
+                    브리오쉬 번, 스페셜 스모키 소스가 선사하는 놀랍도록 새로운
+                    맛의 치킨 버거!
                   </p>
                 </StCardText>
               </div>
             </div>
           </div>
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1583727855319.png"
                 class="card-img-top"
@@ -68,9 +65,6 @@ function Menu() {
                 <StCardText>
                   <p class="card-text">
                     100% 순 쇠고기 패티 두 장에 빅맥만의 특별한 소스. <br></br>
-                    입안에서 살살 녹는 치즈와 신선한 양상추, 양파, 그리고
-                    피클까지.
-                    <br></br>
                     50년 넘게 전 세계인의 입맛을 사로 잡은 버거의 대명사.
                   </p>
                 </StCardText>
@@ -78,7 +72,7 @@ function Menu() {
             </div>
           </div>
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1583728352960.png"
                 class="card-img-top"
@@ -90,16 +84,16 @@ function Menu() {
                 </StCardTitle>
                 <StCardText>
                   <p class="card-text">
-                    매콤한 시즈닝을 입힌 100% 닭가슴 통살 위에 <br></br>
-                    아삭아삭한 양상추와 신선한 토마토~ 겉은 바삭, 속은 부드러운{" "}
-                    <br></br>치킨 패티의 매콤함으로 입맛도 기분도 화끈하게!
+                    매콤한 시즈닝을 입힌 100% 닭가슴 통살 위에 아삭아삭한
+                    양상추와 신선한 토마토~ 겉바속촉 치킨 패티로 입맛도 기분도
+                    화끈하게!
                   </p>
                 </StCardText>
               </div>
             </div>
           </div>
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1583727649835.png"
                 class="card-img-top"
@@ -111,16 +105,15 @@ function Menu() {
                 </StCardTitle>
                 <StCardText>
                   <p class="card-text">
-                    든든한 맥치킨에 골든 모짜렐라 치즈 스틱 2개와 <br></br>
-                    매콤한 아라비아따 소스를 더해 강렬하게 재탄생한 맥치킨
-                    모짜렐라!
+                    든든한 맥치킨에 골든 모짜렐라 치즈 스틱 2개와 매콤한
+                    아라비아따 소스를 더해 강렬하게 재탄생한 맥치킨 모짜렐라!
                   </p>
                 </StCardText>
               </div>
             </div>
           </div>
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1599119647587.png"
                 class="card-img-top"
@@ -132,16 +125,15 @@ function Menu() {
                 </StCardTitle>
                 <StCardText>
                   <p class="card-text">
-                    113g 두툼한 순 쇠고기 패티, <br></br>특별한 1955 소스에 깊은
-                    풍미의 그릴드 어니언까지! <br></br>
-                    1955년의 맛을 담은 영원한 오리지널 1955 버거
+                    113g 두툼한 순 쇠고기 패티, 특별한 소스에 깊은 풍미의 그릴드
+                    어니언까지! 1955년의 맛을 담은 영원한 오리지널 버거
                   </p>
                 </StCardText>
               </div>
             </div>
           </div>
           <div class="col">
-            <div class="card h-100">
+            <div class="card h-90">
               <img
                 src="https://www.mcdonalds.co.kr/upload/product/pcfile/1599119647587.png"
                 class="card-img-top"
@@ -155,9 +147,8 @@ function Menu() {
                 <StCardText>
                   <p class="card-text">
                     두 장의 100% 순 쇠고기 패티와 그릴에 구운 1장의 베이컨을
-                    얹고, <br></br>신선한 토마토와 양상추, 매콤달콤한 스위트
-                    칠리 소스, 치즈, 마요네즈를 더해 더욱 풍부하고 신선한 맛의
-                    프리미엄 버거.
+                    얹고, 신선한 토마토와 양상추, 매콤달콤한 스위트 칠리 소스,
+                    치즈, 마요네즈를 더한 프리미엄 버거.
                   </p>
                 </StCardText>
               </div>
@@ -165,6 +156,9 @@ function Menu() {
           </div>
         </div>
       </StMenuContainer>
+      <StCartBox>
+        <div>장바구니 자리</div>
+      </StCartBox>
     </div>
   );
 }
@@ -186,4 +180,13 @@ const StCardText = styled.p`
   font-weight: bold;
   color: #868686;
   font-family: "NanumSquareRoundBold";
+`;
+
+const StCartBox = styled.div`
+  /* border: 2px solid #343333; */
+  background-color: #3d3d3d97;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 240px;
 `;
