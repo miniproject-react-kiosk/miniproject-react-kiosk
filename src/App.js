5171202c1c03e489fda45bfcd0b201ae96f4305e
yrk721 <yoorae0721@naver.com>
2023-01-17 13:32:18 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-// import React, { Component } from "react";
-// import PhoneNumberForm from "./components/PhoneNumberForm";
+import React, { Component } from "react";
+import PhoneNumberForm from "./components/PhoneNumberForm";
 // import Router from "./shared/Router";
 
-function App() {
-  return <>{<Outlet />}</>;
-}
-
-// class App extends Component {
-//   handleCreate = (data) => {
-//     console.log(data);
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <PhoneNumberForm onCreate={this.handleCreate} />
-//       </div>
-//     );
-//   }
+// function App() {
+//   return <>{<Outlet />}</>;
 // }
+
+class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <>{<Outlet />}</>;
+        </div>
+        <PhoneNumberForm onCreate={this.handleCreate} />
+      </div>
+    );
+  }
+}
 
 export default App;
