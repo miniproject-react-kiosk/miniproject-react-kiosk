125a9822fab873fb863bb4bed9e2e3c0dcdf792e
yrk721 <yoorae0721@naver.com>
2023-01-17 16:21:36 +0900

    PhoneNumberForm Component action 연결 완료

--- src/components/PhoneNumberForm.jsx
+++ src/components/PhoneNumberForm.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function PhoneNumberForm() {
+function PhoneNumberForm({ handleAction, actionTitle }) {
   const [numberValue, setNumberValue] = useState("");
+  const [title, setTitle] = useState(actionTitle);
 
   const regPhone = (data) => {
     data = data.replace(/[^0-9]/g, "");
@@ -16,74 +17,55 @@ function PhoneNumberForm() {
     setNumberValue(regPhone(value));
   };
 
-  // const onReset = () => {
-  //   setNumberValue("");
-  // };
+  const onReset = () => {
+    setNumberValue("");
+  };
+
+  useEffect(() => {
+    setTitle(actionTitle);
+  }, []);
 
   return (
     <div>
       <ContentBox>
-        {/* <div>Phone Number</div> */}
         <ContentInput
           name="numberValue"
           value={numberValue || ""}
           placeholder="전화번호를 입력해주세요."
           onChange={handleNumber}
         />
-        {/* <button onClick={onReset}>Reset</button> */}
+        <StButton>
+          <button
+            type="button"
+            class="btn btn-outline-secondary"
+            onClick={() => {
+              onReset();
+              handleAction();
+            }}
+          >
+            {title}
+          </button>
+        </StButton>
       </ContentBox>
     </div>
   );
 }
 
-// class PhoneNumberForm extends Component {
-//   state = {
-//     name: '',
-//     phone: ''
-//   }
-//   handleChange = (e) => {
-//     this.setState({
-//       [e.target.name]: e.target.value
-//     })
-//   }
-//   handleSubmit = (e) => {
-//     // 페이지 리로딩 방지
-//     e.preventDefault();
-//     // 상태값을 onCreate 를 통하여 부모에게 전달
-//     this.props.onCreate(this.state);
-//     // 상태 초기화
-//     this.setState({
-//       name: '',
-//       phone: ''
-//     })
-//   }
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <input
-//           placeholder="전화번호"
-//           value={this.state.phone}
-//           onChange={this.handleChange}
-//           name="phone"
-//         />
-//         <button type="submit">등록</button>
-//       </form>
-//     );
-//   }
-// }
-
 export default PhoneNumberForm;
 
 const ContentBox = styled.div`
+  display: flex;
+  justify-content: center;
   text-align: center;
   font-family: "NanumSquareRoundBold";
   font-size: 16px;
 `;
 const ContentInput = styled.input`
-  /* display: flex; */
-  /* margin: auto; */
-  /* justify-content: center; */
   text-align: center;
   width: 25vw;
   height: 38px;
+`;
+
+const StButton = styled.div`
+  margin-left: 10px;
 `;
