85de7f7736ca54df915dd218203d8ad1ecfb3cf7
yrk721 <yoorae0721@naver.com>
2023-01-17 13:26:43 +0900

    PhoneNumberForm 수정

--- src/components/PhoneNumberForm.jsx
+++ src/components/PhoneNumberForm.jsx
@@ -16,15 +16,21 @@ function PhoneNumberForm() {
     setNumberValue(regPhone(value));
   };
 
+  // const onReset = () => {
+  //   setNumberValue("");
+  // };
+
   return (
     <div>
       <ContentBox>
-        <div>Phone Number</div>
+        {/* <div>Phone Number</div> */}
         <ContentInput
           name="numberValue"
           value={numberValue || ""}
+          placeholder="전화번호를 입력해주세요."
           onChange={handleNumber}
         />
+        {/* <button onClick={onReset}>Reset</button> */}
       </ContentBox>
     </div>
   );
@@ -32,5 +38,16 @@ function PhoneNumberForm() {
 
 export default PhoneNumberForm;
 
-const ContentBox = styled.div``;
-const ContentInput = styled.input``;
+const ContentBox = styled.div`
+  text-align: center;
+  font-family: "NanumSquareRoundBold";
+  font-size: 16px;
+`;
+const ContentInput = styled.input`
+  /* display: flex; */
+  /* margin: auto; */
+  /* justify-content: center; */
+  text-align: center;
+  width: 25vw;
+  height: 38px;
+`;
