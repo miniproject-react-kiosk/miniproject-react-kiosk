37a273a94f7e482fe625f7ed4bdad9a2323bfe0f
yrk721 <yoorae0721@naver.com>
2023-01-18 16:03:39 +0900

    CartList / 결제창 분리

--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -59,10 +59,10 @@ const StCartList = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-
+  margin-top: 10pt;
   align-items: center;
-  padding: 10px;
-  height: 80px;
+  padding: 20px;
+  height: 40px;
 `;
 
 const StCartListMenu = styled.div`
@@ -70,43 +70,37 @@ const StCartListMenu = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border: solid 2px #c0c0c0;
   border-radius: 10px;
-  height: 60px;
+  height: 40px;
   background-color: #c0c0c0;
   padding: 20px;
+  font-family: "NanumSquareRoundBold";
 `;
 
 const StCartListControl = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  /* border: solid 2px gray; */
-
   align-items: center;
   flex-basis: 30%;
-  height: 60px;
+  font-weight: bold;
+  /* height: 40px; */
   /* margin: 0 20px 0 20px; */
 `;
 
 const StCartListControlBox2 = styled.div`
-  margin: 5px;
-  border: solid 2px #c0c0c0;
   border-radius: 10px;
-  height: 60px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-basis: 22%;
-
   background-color: #c0c0c0;
 `;
 
 const StCartListControlBox = styled.div`
-  margin: 5px;
-  border: solid 2px white;
   border-radius: 10px;
-  height: 60px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -115,7 +109,6 @@ const StCartListControlBox = styled.div`
   cursor: pointer;
   &:hover {
     border: solid 2px #ffe600e2;
-
     background-color: #ffe600e2;
     transition: all 300ms ease;
     /* transform: rotate(-30deg) scale(1.2); */
@@ -127,15 +120,13 @@ const StCartListDelete = styled.div`
   align-items: center;
   justify-content: center;
   flex-basis: 10%;
-  border: solid 2px white;
   border-radius: 10px;
-  height: 60px;
+  height: 40px;
   background-color: white;
-  font-size: 40px;
+  font-size: 25px;
   cursor: pointer;
   &:hover {
     border: solid 2px #ffe600e2;
-
     background-color: #ffe600e2;
     transition: all 300ms ease;
     /* transform: rotate(-30deg) scale(1.2); */
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,18 +1,18 @@
 //2번페이지
 
-import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import styled from 'styled-components';
-import { BsCartCheck } from 'react-icons/bs';
-import { BiUndo } from 'react-icons/bi';
-import { FaWonSign } from 'react-icons/fa';
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import styled from "styled-components";
+import { BsCartCheck } from "react-icons/bs";
+import { BiUndo } from "react-icons/bi";
+import { FaWonSign } from "react-icons/fa";
 // import AddCart from "../components/AddCart";
 
 // import BackButton from "../components/BackButton";
-import MenuList from '../components/MenuList';
-import CartList from '../components/CartList';
-import { useSelector } from 'react-redux';
+import MenuList from "../components/MenuList";
+import CartList from "../components/CartList";
+import { useSelector } from "react-redux";
 
 function Menu() {
   const navigate = useNavigate();
@@ -45,49 +45,48 @@ function Menu() {
         <div>
           <CartList />
         </div>
-
-        <StBuyOrResetBox>
-          <StCartTotalPrice>
-            <div class='d-grid gap-2 col-10 mx-auto'>
-              <div class='btn btn-outline-light btn-lg'>
-                Total : {totalprice} <FaWonSign />
-              </div>
-            </div>
-          </StCartTotalPrice>
-
-          {/* 취소하기 버튼 */}
-          <StResetButton>
-            <div class='d-grid gap-2 col-10 mx-auto'>
-              <button
-                type='button'
-                class='btn btn-outline-light btn-lg'
-                onClick={() => {
-                  navigate(-1);
-                }}
-              >
-                {' '}
-                <BiUndo className='Undo' /> 취소하기
-              </button>
-            </div>
-          </StResetButton>
-
-          {/* 결제하기 버튼 */}
-          <StBuyButton>
-            <div class='d-grid gap-2 col-10 mx-auto'>
-              <button
-                type='button'
-                class='btn btn-outline-light btn-lg'
-                onClick={() => {
-                  navigate(`/Menu/OrderCheck/${param.takeOutId}`);
-                }}
-              >
-                {' '}
-                <BsCartCheck className='Order' /> 주문하기
-              </button>
-            </div>
-          </StBuyButton>
-        </StBuyOrResetBox>
       </StCartBox>
+      <StBuyOrResetBox>
+        <StCartTotalPrice>
+          <div class="d-grid gap-2 col-10 mx-auto">
+            <div class="btn btn-outline-light btn-lg">
+              Total : {totalprice} <FaWonSign />
+            </div>
+          </div>
+        </StCartTotalPrice>
+
+        {/* 취소하기 버튼 */}
+        <StResetButton>
+          <div class="d-grid gap-2 col-10 mx-auto">
+            <button
+              type="button"
+              class="btn btn-outline-light btn-lg"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              {" "}
+              <BiUndo className="Undo" /> 취소하기
+            </button>
+          </div>
+        </StResetButton>
+
+        {/* 결제하기 버튼 */}
+        <StBuyButton>
+          <div class="d-grid gap-2 col-10 mx-auto">
+            <button
+              type="button"
+              class="btn btn-outline-light btn-lg"
+              onClick={() => {
+                navigate(`/Menu/OrderCheck/${param.takeOutId}`);
+              }}
+            >
+              {" "}
+              <BsCartCheck className="Order" /> 주문하기
+            </button>
+          </div>
+        </StBuyButton>
+      </StBuyOrResetBox>
     </div>
   );
 }
@@ -105,25 +104,27 @@ const StHeader = styled.div`
 
 const StTitle = styled.h1`
   text-align: center;
-  font-family: 'MorningBreezeBold';
+  font-family: "MorningBreezeBold";
   margin-top: 15px;
   color: #2a2a2a;
 `;
 
 const StMenuContainer = styled.div`
-  display: flex flex-directio;
+  display: flex;
   z-index: 0;
   padding: 35px;
   margin-top: 60px;
 `;
 
 const StCartBox = styled.div`
-  border-radius: 10px;
+  border-radius: 5px;
   background-color: #23232399;
   position: fixed;
+  margin-bottom: 100px;
+  padding: 10pt;
   bottom: 0;
   width: 100%;
-  height: 300px;
+  height: 200px;
   overflow-y: auto;
   //   //뭔진모르겠는데 스크롤 숨기는 기능임...
   &::-webkit-scrollbar {
@@ -135,45 +136,27 @@ const StCartBox = styled.div`
 `;
 
 const StBuyOrResetBox = styled.div`
-  /* background-color: #3d3d3d61; */
-  background-color: #0000005f;
-
+  background-color: #232323f2;
   border-radius: 10px;
+  display: flex;
   position: fixed;
   bottom: 0;
   width: 100%;
   height: 100px;
-  display: flex;
   align-items: center;
 `;
 
 const StCartTotalPrice = styled.div`
   flex-basis: 50%;
-  /* position: absolute;
-  right: 0%;
-  width: 25vw;
-  height: 20px;
-  justify-content: space-between;
-  font-family: 'NanumSquareRoundBold'; */
+  font-family: "NanumSquareRoundBold";
 `;
 
 const StBuyButton = styled.div`
   flex-basis: 25%;
-  /* position: absolute;
-  right: 0%;
-  width: 25vw;
-  height: 20px;
-  justify-content: space-between;
-  font-family: 'NanumSquareRoundBold'; */
+  font-family: "NanumSquareRoundBold";
 `;
 
 const StResetButton = styled.div`
   flex-basis: 25%;
-
-  /* position: absolute;
-  right: 23%;
-  width: 25vw;
-  height: 20px;
-  justify-content: space-between;
-  font-family: 'NanumSquareRoundBold'; */
+  font-family: "NanumSquareRoundBold";
 `;
