7512a9df9a6f3aead5a1acb207c1d5a0ce8834fe
yrk721 <yoorae0721@naver.com>
2023-01-21 21:35:24 +0900

    관리자 버튼 생성

---
+++ src/components/AdminButton.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import styled from "styled-components";
+
+function AdminButton() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <StAdminButtonContainer>
+        <div class="d-grid gap-2 col-1 mx-auto">
+          <button
+            type="button"
+            class="btn btn-outline-secondary btn-lg"
+            onClick={() => {
+              navigate(`/Admin`);
+              // navigate(`/PasswordProtectedPage/Admin`);
+            }}
+          >
+            <StAdminButton>
+              <MdOutlineManageAccounts></MdOutlineManageAccounts>
+            </StAdminButton>
+          </button>
+        </div>
+      </StAdminButtonContainer>
+    </div>
+  );
+}
+
+export default AdminButton;
+
+const StAdminButtonContainer = styled.div`
+  margin-top: 10px;
+  padding-right: 10px;
+  position: fixed;
+  right: 5%;
+`;
+
+const StAdminButton = styled.div`
+  font-size: 20pt;
+`;
