1a33005a1f013f816044686c873142321142cfb6
yrk721 <yoorae0721@naver.com>
2023-01-22 23:44:35 +0900

    Update PasswordProtectedPage.jsx

--- src/components/PasswordProtectedPage.jsx
+++ src/components/PasswordProtectedPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Home from "../pages/Home";
-import Admin from "../pages/Admin";
+import styled from "styled-components";
+import swal from "sweetalert";
 
 function PasswordProtectedPage() {
   const [password, setPassword] = useState("");
@@ -9,30 +9,39 @@ function PasswordProtectedPage() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // 서버에서 비밀번호를 검증
-    // 예를 들어, 기본 비밀번호가 "password"라면,
-    if (password === "hh5kiosk") {
+
+    if (password === "hhkiosk") {
       setIsValid(true);
+      swal("관리자 로그인 성공!", "관리자 페이지로 이동합니다. ", "info");
+    } else {
+      swal("관리자 로그인 실패!", "비밀번호를 다시 입력해주세요.", "error");
     }
   };
-
   return (
     <div>
-      {!isValid && (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      )}
-      {isValid && <p>Welcome! You have entered the correct password.</p>}
+      <StContainer>
+        {!isValid && (
+          <form onSubmit={handleSubmit}>
+            <label>
+              PW :{" "}
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+        {isValid && <p>당일매출, 총매출 등 관리자용 API가 들어올 자리!</p>}
+      </StContainer>
     </div>
   );
 }
 
 export default PasswordProtectedPage;
+
+const StContainer = styled.div`
+  font-family: "NanumSquareRoundBold";
+  font-size: 14pt;
+`;
