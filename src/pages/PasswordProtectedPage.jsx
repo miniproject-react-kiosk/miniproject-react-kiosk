9d6956894d80a7373750295d011b8615b0e7bb07
yrk721 <yoorae0721@naver.com>
2023-01-24 07:31:55 +0900

    PasswordProtectedPage 분리

--- src/pages/PasswordProtectedPage.jsx
+++ src/pages/PasswordProtectedPage.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import axios from "axios";
 import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
 function PasswordProtectedPage() {
   const [password, setPassword] = useState("");
   const [isValid, setIsValid] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,29 +14,15 @@ function PasswordProtectedPage() {
 
     if (password === process.env.REACT_APP_ADMIN_PW) {
       setIsValid(true);
-      swal("관리자 로그인 성공!", "관리자 페이지로 이동합니다. ", "info");
+      swal("관리자 로그인 성공!", "관리자 페이지로 이동합니다. ", "info").then(
+        () => {
+          navigate(`/Admin`);
+        }
+      );
     } else {
       swal("관리자 로그인 실패!", "비밀번호를 다시 입력해주세요.", "error");
     }
   };
-
-  const [dailySales, setDailySales] = useState();
-
-  useEffect(() => {
-    axios
-      .get(
-        "http://13.209.12.254/order/dailySales",
-        {},
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        setDailySales(response.data);
-      });
-  }, []);
-
-  console.log("******", dailySales);
 
   return (
     <div>
@@ -53,9 +40,7 @@ function PasswordProtectedPage() {
             <button type="submit">Submit</button>
           </form>
         )}
-        {isValid && (
-          <p>당일매출, 총매출 등 관리자용 API가 들어올 자리! {dailySales}</p>
-        )}
+        {isValid && <p></p>}
       </StContainer>
     </div>
   );
