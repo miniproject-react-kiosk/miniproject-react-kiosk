936eacd532cd4f67506e10be3684bf0b4df746a8
yrk721 <yoorae0721@naver.com>
2023-01-24 03:45:18 +0900

    관리자 비밀번호 환경변수로 설정

--- src/components/PasswordProtectedPage.jsx
+++ src/components/PasswordProtectedPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import swal from "sweetalert";
 
 function PasswordProtectedPage() {
@@ -10,13 +11,32 @@ function PasswordProtectedPage() {
     e.preventDefault();
     // 서버에서 비밀번호를 검증
 
-    if (password === "hhkiosk") {
+    if (password === process.env.REACT_APP_ADMIN_PW) {
       setIsValid(true);
       swal("관리자 로그인 성공!", "관리자 페이지로 이동합니다. ", "info");
     } else {
       swal("관리자 로그인 실패!", "비밀번호를 다시 입력해주세요.", "error");
     }
   };
+
+  const [dailySales, setDailySales] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://13.209.12.254/order/dailySales",
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        setDailySales(response.data);
+      });
+  }, []);
+
+  console.log("******", dailySales);
+
   return (
     <div>
       <StContainer>
@@ -33,7 +53,9 @@ function PasswordProtectedPage() {
             <button type="submit">Submit</button>
           </form>
         )}
-        {isValid && <p>당일매출, 총매출 등 관리자용 API가 들어올 자리!</p>}
+        {isValid && (
+          <p>당일매출, 총매출 등 관리자용 API가 들어올 자리! {dailySales}</p>
+        )}
       </StContainer>
     </div>
   );
